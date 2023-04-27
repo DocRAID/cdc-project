6f2221832f0d8eaf388fbf95753fc6c6f81b43cc
DocRAID <l01062506145@gmail.com>
2023-04-27 15:50:47 +0900

    first

---
+++ CMakeFiles/poco_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poco_test.dir/main.cpp.o"
+  "CMakeFiles/poco_test.dir/main.cpp.o.d"
+  "poco_test"
+  "poco_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poco_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
